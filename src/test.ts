6a842edba5621b426e1942bf03991d438646d7e2
Mythical Forest Collective <Random.Access.Exomemory@gmail.com>
2022-03-16 19:58:21 +0000

    Current shoddy implementation of argv as well as a few new tests

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,11 @@
-import {process} from "./index";
-import {os} from "./index";
+import {process, os} from "./index";
 
 console.log(process.arch);
+console.log(process.argv);
+console.log(process.argv.length);
 console.log(os.arch());
 console.log(os.EOL);
 console.log(os.endianness());
 console.log(os.freemem());
+
+// process.abort(); // Worked as expected
