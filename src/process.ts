ca8678dbba26157526127765070e6aad356d0f90
Mythical Forest Collective <Random.Access.Exomemory@gmail.com>
2022-03-16 17:06:18 +0000

    Update the .gitignore to be more extensive and make process.arch use a switch case so we can return the arch nodejs programs expect

--- src/process.ts
+++ src/process.ts
@@ -4,6 +4,12 @@ const System = java.type("java.lang.System");
 
 export default new class process {
     get arch(): string {
-        return System.getProperty("os.arch");
+        switch(System.getProperty("os.arch")) {
+            case 'aarch64':
+                return 'arm64';
+            default:
+                console.warn("This architecture isn't registered in the switch statement! Please make an issue on the github repo with the architecture used! `"+System.getProperty("os.arch")+"`");
+                return System.getProperty("os.arch");
+        }
     }
 }
